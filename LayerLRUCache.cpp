53afff4fd1dabc4172a28cf6f3dffe9b08078827
Baldwin <xuebingbing100@163.com>
2015-06-26 09:39:15 +0800

    mutex pass

--- LayerLRUCache.cpp
+++ LayerLRUCache.cpp
@@ -11,38 +11,11 @@
 
 using namespace thp;
 
-TBundleNo::TBundleNo() 
-{
-	unLv = 0;
-	unBunldeID = 0;
-}
-
-bool TBundleNo::operator ==(const TBundleNo& tNoNew) const
-{
-	if(tNoNew.unLv == unLv && unBunldeID == tNoNew.unBunldeID)
-		return true;
-
-	return false;
-}
-
-bool TBundleNo::operator <(const TBundleNo& tNoNew) const
-{
-	if( unLv < tNoNew.unLv )
-		return true;
-	
-	if(unLv == tNoNew.unLv && unBunldeID < tNoNew.unBunldeID)
-		return true;
-
-	return false;
-}
-
 LayerLRUCache::LayerLRUCache(unsigned int unCapacity) 
 {
-	//QWriteLocker locker( &m_qrwMutex );
-
 	//pthread_rwlock_init(&m_prwMutex, NULL);
 
-	pthread_mutex_init(&m_plock, NULL);
+	pthread_mutex_init(&m_ptMutex, NULL);
 
 	m_lockReadTimes = 0;
 	m_lockWriteTimes = 0;
@@ -53,26 +26,31 @@ LayerLRUCache::LayerLRUCache(unsigned int unCapacity)
 	m_unUsedKBCount = 0;
 	m_unLruHeadBundleKBCount = 0;
 
-	if( bLocked)
-		unlock(false);
+	unlock(false);
 }
 
 void LayerLRUCache::setCapacity(unsigned int unCapacity)
 {
-	//QWriteLocker locker( &m_qrwMutex );
 	bool bLocked = lockForWrite();
+	if(bLocked)
+		return ;
 
 	m_unMaxKBCount = unCapacity;
 
-	if( bLocked)
-		unlock(false);
+	unlock(false);
 }
 
 unsigned int LayerLRUCache::getCapacity()
 {
-	return 1024 * 1024;
-	//QReadLocker locker( &m_qrwMutex );
-	//return m_unMaxKBCount;
+	bool bLocked = lockForWrite();
+	if(bLocked)
+		return 0;
+
+	int nRes = m_unMaxKBCount;
+
+	unlock(false);
+
+	return nRes;
 }
 
 Bundle* LayerLRUCache::get(const TBundleNo& key)
@@ -89,41 +67,42 @@ Bundle* LayerLRUCache::get(const TBundleNo& key)
 
 Bundle* LayerLRUCache::get_pri(const TBundleNo& key)
 {
-	int i = 0;
+	{
+		bool bLocked = lockForRead();
+		if(!bLocked)
+			return NULL;
+
+		std::map<TBundleNo, std::list< std::pair<TBundleNo, Bundle*> >::iterator >::iterator it = m_mp.find(key);
+
+		//没有命中
+		if(it == m_mp.end())      
+		{
+			unlock(false);
+			lockForWrite();
+			return NULL;
+		}
+
+		// 最近一次使用
+		std::list< std::pair<TBundleNo, Bundle*> >::iterator listIt = it->second;
+		if( listIt == m_listCache.begin() )
+		{
+			Bundle* pBundleRes = m_listCache.begin()->second;
+			pBundleRes->lockForRead();
+
+			unlock(false);
+
+			return pBundleRes;
+		}
+
+		unlock(true);
+	}
 
 	bool bLocked = lockForWrite();
-	if(!bLocked)
+	if( !bLocked )
 		return NULL;
-	//bool bLocked = lockForRead();
 
 	std::map<TBundleNo, std::list< std::pair<TBundleNo, Bundle*> >::iterator >::iterator it = m_mp.find(key);
-
-	//没有命中
-	if(it == m_mp.end())      
-	{
-		unlock(false);
-		return NULL;
-	}
-
-	// 最近一次使用
 	std::list< std::pair<TBundleNo, Bundle*> >::iterator listIt = it->second;
-	if( listIt == m_listCache.begin() )
-	{
-		Bundle* pBundleRes = m_listCache.begin()->second;
-		pBundleRes->lockForRead();
-
-		unlock(false);
-
-		return pBundleRes;
-	}
-
-	//if( bLocked )
-	//	unlock(true);
-
-	//bLocked = lockForWrite();
-	
-	it = m_mp.find(key);
-	listIt = it->second;
 
 	// 命中节点	
 	std::pair<TBundleNo, Bundle*> pairNode;
@@ -153,7 +132,7 @@ void thp::LayerLRUCache::set(const TBundleNo& key, thp::Bundle* pBundle)
 	}
 	__except(EXCEPTION_EXECUTE_HANDLER)
 	{
-		;//mzRunLog::instance().write_especial_log(GetExceptionCode(), __FILE__, __FUNCTION__, __LINE__);
+		;
 	}
 }
 
@@ -233,16 +212,61 @@ void LayerLRUCache::set_pri(const TBundleNo& key, Bundle* pBundle)
 	}
 }
 
+void thp::LayerLRUCache::push(const TBundleNo& key, thp::Bundle* pBundle)
+{
+	std::set<Bundle*> setRelease;
+
+	std::map<TBundleNo, std::list< std::pair<TBundleNo, Bundle*> >::iterator >::iterator it = m_mp.find(key);
+
+	m_unUsedKBCount += pBundle->getMemKB();
+
+	// 栈满了
+	while(m_unUsedKBCount > m_unMaxKBCount)
+	{
+		if( 0 == m_listCache.size() )
+			break;
+
+		const std::pair<TBundleNo, Bundle*>& pairNode = m_listCache.back();
+
+		// 内部保证没有出现负值的情况
+		m_mp.erase( pairNode.first );
+
+		m_unUsedKBCount -= pairNode.second->getMemKB();
+
+		Bundle* pBl = m_listCache.back().second;
+
+		setRelease.insert(pBl);
+
+		// 出栈
+		m_listCache.pop_back();
+	}
+
+	std::pair<TBundleNo, Bundle*> pairNewNode(key, pBundle);
+	m_listCache.push_front( std::make_pair(key, pBundle) );
+	m_mp[key] = m_listCache.begin();
+
+	unlock(false);
+
+	for (std::set<Bundle*>::iterator setit = setRelease.begin(); setit != setRelease.end(); ++setit)
+	{
+		Bundle* pDl = *setit;
+		pDl->lockForWrite();
+		delete pDl;
+		pDl = NULL;
+		// ? 析构了的对象的锁还要不要释放
+	}
+}
+
 #include <Windows.h>
 
 bool thp::LayerLRUCache::lockForRead()
 {
-	RAW_LOG(INFO, "locking for read");
-	if( 0 == pthread_mutex_lock(&m_plock) )
+	RAW_LOG(INFO, "locking for read, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
+	if( 0 == pthread_mutex_lock(&m_ptMutex) )
 	//if( 0 == pthread_rwlock_tryrdlock(&m_prwMutex) )
 	{
 		InterlockedIncrement(&m_lockReadTimes);
-		RAW_LOG(INFO, "lock read success");
+		RAW_LOG(INFO, "lock read success, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 		return true;
 	}
 	else
@@ -254,13 +278,13 @@ bool thp::LayerLRUCache::lockForRead()
 
 bool thp::LayerLRUCache::lockForWrite()
 {
-	RAW_LOG(INFO, "locking for write %d times, rd %d", m_lockWriteTimes, m_lockReadTimes);
+	RAW_LOG(INFO, "locking for write,wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 
-	if( 0 == pthread_mutex_lock(&m_plock) )
+	if( 0 == pthread_mutex_lock(&m_ptMutex) )
 	//if(0 == pthread_rwlock_wrlock(&m_prwMutex) )
 	{
 		InterlockedIncrement(&m_lockWriteTimes);
-		RAW_LOG(INFO, "lock write success %d", m_lockWriteTimes);
+		RAW_LOG(INFO, "lock write success,wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 		return true;
 	}
 	else
@@ -272,19 +296,19 @@ bool thp::LayerLRUCache::lockForWrite()
 
 void thp::LayerLRUCache::unlock(bool bRead)
 {
-	RAW_LOG(INFO, "unlocking, remain %d times, wt %d", m_lockReadTimes, m_lockWriteTimes);
-	if( 0 == pthread_mutex_unlock(&m_plock) )
+	RAW_LOG(INFO, "unlocking, rd %d , wt %d", m_lockReadTimes, m_lockWriteTimes);
+	if( 0 == pthread_mutex_unlock(&m_ptMutex) )
 	//if( 0 == pthread_rwlock_unlock(&m_prwMutex) )
 	{
 		if( bRead )
 		{
 			InterlockedDecrement(&m_lockReadTimes);
-			RAW_LOG(INFO, "unlock read success, remain %d times", m_lockReadTimes);
+			RAW_LOG(INFO, "unlock read success, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 		}
 		else
 		{
 			InterlockedDecrement(&m_lockWriteTimes);
-			RAW_LOG(INFO, "unlock write success, remain %d times", m_lockWriteTimes);
+			RAW_LOG(INFO, "unlock write success, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 		}
 	}
 	else
@@ -296,7 +320,11 @@ void thp::LayerLRUCache::unlock(bool bRead)
 	}
 }
 
-
+thp::LayerLRUCache::~LayerLRUCache()
+{
+	//pthread_rwlock_destroy(&m_prwMutex);
+	pthread_mutex_destroy(&m_ptMutex);
+}
 
 void LayerLRUCache::updateUsedCapacity()
 {
