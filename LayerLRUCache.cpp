41310aec7f11b4fdd1e3af0a748bf1a0fb45f851
Baldwin <xuebingbing100@163.com>
2015-07-09 16:01:53 +0800

    替换qt读写锁前

--- LayerLRUCache.cpp
+++ LayerLRUCache.cpp
@@ -1,26 +1,25 @@
+#include "StdAfx.h"
 #include "LayerLRUCache.h"
 #include "Bundle.h"
-#include "glog/logging.h"
-#include "glog/raw_logging.h"
+//#include "glog/logging.h"
+//#include "glog/raw_logging.h"
 #include <set>
-#include "CircularList.h"
-#include "BundleFactory.h"
-
+#include "dclist.h"
+#include  <CLogThreadMgr.h>
 #ifdef _DEBUG
 #include <ctime>
 #include <Windows.h>
 #endif
 
 using namespace thp;
-
 LayerLRUCache::LayerLRUCache(unsigned int unCapacity) 
 {
 	pthread_rwlock_init(&m_prwMutex, NULL);
 
-	//pthread_mutex_init(&m_ptMutex, NULL);
-
+#ifdef _DEBUG
 	m_lockReadTimes = 0;
 	m_lockWriteTimes = 0;
+#endif
 
 	bool bLocked = lockForWrite();
 
@@ -28,22 +27,49 @@ LayerLRUCache::LayerLRUCache(unsigned int unCapacity)
 	m_unUsedKBCount = 0;
 	m_unLruHeadBundleKBCount = 0;
 
-	m_pList = new CircularList;
-
+	//m_pBundles = NULL;
+   _initLogWriter();
 	unlock(false);
+}
+
+bool LayerLRUCache::_initLogWriter()
+{
+	// 获取写日志对象
+	m_pLogWriter = CLogThreadMgr::instance()->getLogWriter("LayerLRUCache.log");
+
+	// 如果不存在，创建日志文件，加载日志配置
+	if (m_pLogWriter == NULL)
+	{
+		CLogAppender * pLogAppender = new CLogAppender("LayerLRUCache", "LayerLRUCache.log", "", "DebugLog"); 
+
+		// 获取写日志对象
+		m_pLogWriter = CLogThreadMgr::instance()->createLogWriter(pLogAppender);
+	}
+	return true; 
 }
 
 thp::LayerLRUCache::~LayerLRUCache()
 {
+	// 释放hash表资源
+	lockForWrite();
+
+	m_pLogWriter->debugLog("LRU 正在释放资源...");
+	//LOG(INFO) << "LRU 正在释放资源...";
+
+	unlock(false);
+
 	pthread_rwlock_destroy(&m_prwMutex);
-	delete m_pList;
-	//pthread_mutex_destroy(&m_ptMutex);
+	//delete m_pList;
+
+	m_pLogWriter->debugLog("LRU 完成释放资源...");
+	//LOG(INFO) << "LRU 完成释放资源";
 }
+
 
 void LayerLRUCache::setCapacity(unsigned int unCapacity)
 {
 	bool bLocked = lockForWrite();
-	if(bLocked)
+	if( !bLocked )
 		return ;
 
 	m_unMaxKBCount = unCapacity;
@@ -53,208 +79,154 @@ void LayerLRUCache::setCapacity(unsigned int unCapacity)
 
 unsigned int LayerLRUCache::getCapacity()
 {
-	bool bLocked = lockForWrite();
-	if(bLocked)
-		return 0;
-
-	int nRes = m_unMaxKBCount;
-
-	unlock(false);
-
-	return nRes;
+	return m_unMaxKBCount;
 }
-
-Bundle* LayerLRUCache::get(const TBundleNo& key)
-{
-	__try
-	{
-		return get_pri(key);
-	}
-	__except(EXCEPTION_EXECUTE_HANDLER)
-	{
-		return NULL;
-	}
-}
-
-Bundle* LayerLRUCache::get_pri(const TBundleNo& key)
-{
-	lockForRead();
-
-	// 现在的时间复杂度 log2n
-	TbnoBdlMapIt it = m_mapKeyValue.find(key);
-	if( it == m_mapKeyValue.end() )
-	{
-		unlock(true);
-		return NULL;
-	}
-
-	Bundle* pRes = it->second;
-
-	pRes->lockForRead();
-	pRes->heating();
-
-	unlock(true);
-
-	return pRes;
-}
-
-thp::Bundle* thp::LayerLRUCache::addAndGet(const TBundleNo& key, BundleFactory* pFactory)
-{
-	std::set<Bundle*> setRelease;
-	lockForWrite();
-	
-	// 其他线程可能已经写过
-	TbnoBdlMapIt it = m_mapKeyValue.find(key);
-	if( it != m_mapKeyValue.end() )
-	{
-		Bundle* pBundle = it->second;
-		
-		pBundle->heating();
-
-		pBundle->lockForRead();
-
-		unlock(true);
-
-		return pBundle;
-	}
-
-	Bundle* pNewBundle = pFactory->createBundle(key);
-
-	// 这种实现方式最后入栈的bundle过大可能会造成使用的内存使用大于lru的设定值
-	if( m_unUsedKBCount < m_unMaxKBCount )
-	{
-		pNewBundle->cache();
-
-		m_unUsedKBCount += pNewBundle->getMemKB();
-		m_mapKeyValue[key] = pNewBundle;
-		m_pList->push_front(pNewBundle);
-	}// 栈未满 将资源直接插入热段头
-	else
-	{
-		// 资源出栈
-		Bundle* pBundle = (Bundle*)m_pList->tailData();
-		while( m_unUsedKBCount > m_unMaxKBCount )
-		{
-			while ( pBundle->getHeatDegree() > 2 )
-			{
-				pBundle->lockForWrite();
-				pBundle->setHeatDegree(0);
-				pBundle->unlock(false);
-
-				m_pList->moveHeadForward();
-
-				pBundle = (Bundle*)m_pList->tailData();
-			}// 使用两个以上认为是热的
-
-			pBundle = (Bundle*)m_pList->pop_back();
-			m_unUsedKBCount -= pBundle->getMemKB();
-
-			setRelease.insert( pBundle );
-		}// 新资源未加入时保证内存未用满
-	
-		// 缓存资源 
-		pNewBundle->cache();
-
-		// 添加资源到冷端
-		m_unUsedKBCount += pNewBundle->getMemKB();
-		m_mapKeyValue[key] = pNewBundle;
-
-		// map的资源总数 和循环链表中的资源总数相等的
-		int nCount = m_mapKeyValue.size();
-
-		int nColdBegin = 0;
-		
-		// 最多有10个冷资源
-		if( nCount > 20 )
-			nColdBegin = 10;
-		else
-			nColdBegin = nCount >> 1; // <=> nCount/2
-
-		CircularList::TclNode* pColdHead = m_pList->tail();
-		for (int i=0;i<nColdBegin;++i)
-			pColdHead = pColdHead->pPrev;
-
-		m_pList->insert(pColdHead, pNewBundle);
-	}// 栈满
-
-	pNewBundle->heating();
-	pNewBundle->lockForRead();
-
-	unlock(false);
-
-	for (std::set<Bundle*>::iterator setit = setRelease.begin(); setit != setRelease.end(); ++setit)
-	{
-		Bundle* pDl = *setit;
-		pDl->lockForWrite();
-		delete pDl;
-		pDl = NULL;
-		// ? 析构了的对象的锁还要不要释放
-	}
-
-	return pNewBundle;
-}
-
-#include <Windows.h>
 
 bool thp::LayerLRUCache::lockForRead()
 {
-	RAW_LOG(INFO, "locking for read, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
-	//if( 0 == pthread_mutex_lock(&m_ptMutex) )
+#ifndef _DEBUG
+	if( 0 == pthread_rwlock_tryrdlock(&m_prwMutex) )
+		return true;
+	else
+		return false;
+#else
+	m_pLogWriter->debugLog("locking for read");
+	//RAW_DLOG(INFO, "locking for read, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 	if( 0 == pthread_rwlock_tryrdlock(&m_prwMutex) )
 	{
 		InterlockedIncrement(&m_lockReadTimes);
-		RAW_LOG(INFO, "lock read success, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
+		m_pLogWriter->debugLog("locking for read");
+		//RAW_DLOG(INFO, "lock read success, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 		return true;
 	}
 	else
 	{
-		RAW_LOG(INFO, "lock read fail");
+		m_pLogWriter->infoLog("lock read fail");
 		return false;
 	}
+#endif
 }
 
 bool thp::LayerLRUCache::lockForWrite()
 {
-	RAW_LOG(INFO, "locking for write,wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
+#ifndef _DEBUG
+	if(0 == pthread_rwlock_wrlock(&m_prwMutex) )
+		return true;
+	else
+		return false;
+#else
+	//LOG(INFO) << "locking for write,wt"<< m_lockWriteTimes << ", rd" << m_lockReadTimes;
+
+	//RAW_DLOG(INFO, "locking for write,wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
+	//m_pLogWriter->debugLog("lock read fail");
 
 	//if( 0 == pthread_mutex_lock(&m_ptMutex) )
 	if(0 == pthread_rwlock_wrlock(&m_prwMutex) )
 	{
 		InterlockedIncrement(&m_lockWriteTimes);
-		RAW_LOG(INFO, "lock write success,wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
+		//RAW_DLOG(INFO, "lock write success,wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 		return true;
 	}
 	else
 	{
-		RAW_LOG(INFO, "lock write fail");
+		//RAW_DLOG(INFO, "lock write fail");
 		return false;
 	}
+#endif
 }
 
 void thp::LayerLRUCache::unlock(bool bRead)
 {
-	RAW_LOG(INFO, "unlocking, rd %d , wt %d", m_lockReadTimes, m_lockWriteTimes);
+#ifndef _DEBUG
+	if(1 ==	pthread_rwlock_unlock(&m_prwMutex) )
+	{
+		m_pLogWriter->errorLog("资源解锁失败,请重启服务");
+		//RAW_LOG(ERROR, "资源解锁失败,请重启服务");
+	}
+#else
+	//RAW_DLOG(INFO, "unlocking, rd %d , wt %d", m_lockReadTimes, m_lockWriteTimes);
 	//if( 0 == pthread_mutex_unlock(&m_ptMutex) )
 	if( 0 == pthread_rwlock_unlock(&m_prwMutex) )
 	{
 		if( bRead )
 		{
 			InterlockedDecrement(&m_lockReadTimes);
-			RAW_LOG(INFO, "unlock read success, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
+			if(m_lockReadTimes < 0)
+				m_pLogWriter->errorLog("解读锁错误");
+				//LOG(ERROR) << "解读锁错误";
+
+			//RAW_DLOG(INFO, "unlock read success, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 		}
 		else
 		{
 			InterlockedDecrement(&m_lockWriteTimes);
-			RAW_LOG(INFO, "unlock write success, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
+			if(m_lockReadTimes < 0)
+				m_pLogWriter->errorLog("解读锁错误");
+				//LOG(ERROR) << "解读锁错误";
+
+			//RAW_DLOG(INFO, "unlock write success, wt %d, rd %d", m_lockWriteTimes, m_lockReadTimes);
 		}
 	}
 	else
 	{
 		if( bRead )
-			RAW_LOG(INFO, "unlock read fail");
+		{
+			m_pLogWriter->errorLog("unlock read fail");
+			//RAW_DLOG(INFO, "unlock read fail");
+		}
 		else
-			RAW_LOG(INFO, "unlock write fail");
+		{
+			m_pLogWriter->errorLog("unlock write fail");
+			//DLOG(INFO, "unlock write fail");
+			//RAW_DLOG(INFO, "unlock write fail");
+		}
 	}
+#endif
+}
+
+bool thp::LayerLRUCache::isFull()
+{
+	return m_unUsedKBCount >= m_unMaxKBCount;
+}
+
+int thp::LayerLRUCache::add(std::tr1::shared_ptr<Bundle> sp)
+{
+	lockForWrite();
+
+	if(m_unUsedKBCount < m_unMaxKBCount)
+	{
+		m_listHotColdResources.push_front(sp);
+		
+		unlock(false);
+		return 0;
+	}
+	
+	while(m_unUsedKBCount > m_unMaxKBCount)
+	{
+		sPtr sp = m_listHotColdResources.back();
+		while( sp->getTemperature() > 2)
+		{
+			sp->setTemperature(0);
+			m_listHotColdResources.move_head_forward();
+			continue;
+		}
+
+		if( m_unUsedKBCount < sp->getMaxKB() )
+		{
+			m_pLogWriter->errorLog("LRU错误");
+			//LOG(ERROR) << "LRU错误";
+			break;
+		}
+
+		m_unUsedKBCount -= sp->getMaxKB();
+	
+		m_listHotColdResources.pop_back();
+	}
+
+	m_unUsedKBCount += sp->getMaxKB();
+
+	unlock(false);
+	return 0;
 }
 
 
--- LayerLRUCache.h
+++ LayerLRUCache.h
@@ -1,11 +1,15 @@
 #ifndef THP_WMTS_LRUCACHE_H__
 #define THP_WMTS_LRUCACHE_H__
-#include <map>
-#include "pthread/pthread.h"
 #include <Windows.h>
-#include <set>
 #include <list>
+
+#include "pthread/pthread.h"
 #include "ParamDef.h"
+#include "Bundle.h"
+#include "uthash/uthash.h"
+#include "dclist.h"
+
+class CLogWriter;
 
 namespace thp
 {
@@ -13,9 +17,24 @@ class Bundle;
 class BundleFactory;
 class CircularList;
 
+struct TBundleHashTableNode
+{
+	struct thp::TBundleID key;
+	thp::Bundle* val;
+
+	UT_hash_handle hh;
+};
+
 class LayerLRUCache
 {
 public:
+	enum GetType 
+	{
+		G_SUCCESS = 0,
+		G_FAIL,
+	};
+
+	typedef std::tr1::shared_ptr<thp::Bundle> sPtr;
 
 	// 2^32 kb = 2^22MB = 2^12 Gb = 2^2 Tb 
 	// 单位 KB 
@@ -26,22 +45,21 @@ public:
 	unsigned int getCapacity();
 	void setCapacity(unsigned int nCapacity); 
 
-	// 返回使用对象
-	// 返回NULL时对自身加写锁
-	thp::Bundle* get(const TBundleNo& key);
-
 	// 会有一次解锁
 	// 调用者对lru负责加解锁，对象直接放到头部，可能有出栈操作
-	thp::Bundle* addAndGet(const TBundleNo& key, BundleFactory* pFactory);
+	int add( std::tr1::shared_ptr<Bundle> sp );
 
 	bool lockForRead();
 	bool lockForWrite();
 	void unlock(bool bRead);
 
+	bool isFull();
+
+	bool _initLogWriter();
 private:
 
 	// 用于异常捕捉
-	thp::Bundle* get_pri(const TBundleNo& key);
+	thp::Bundle* get_pri(const TBundleIDex& key);
 
 private:
 	
@@ -55,19 +73,22 @@ private:
 	unsigned int m_unMaxKBCount ;
 	unsigned int m_unUsedKBCount;
 	unsigned int m_unLruHeadBundleKBCount;
-	// std::list< std::pair<TBundleNo, thp::Bundle*> > m_listCache;
-	// 使用hash_map替换效率可以更高 
-	typedef std::map<TBundleNo, thp::Bundle*> TbnoBdlMap;
-	typedef TbnoBdlMap::iterator	TbnoBdlMapIt;
 
-	// bundle 资源表
-	TbnoBdlMap m_mapKeyValue;
+	// 资源hash表
+	//TBundleHashTableNode* m_pBundles;
 
 	// 资源热度循环链表 
-	CircularList* m_pList;
+	// CircularList* m_pList;
 
+	//std::map< TBundleID, sPtr > m_Map;
+
+	dclist<sPtr> m_listHotColdResources;
+
+	CLogWriter *					m_pLogWriter;
+#ifdef _DEBUG
 	LONG m_lockReadTimes;
 	LONG m_lockWriteTimes;
+#endif
 };
 
 }
