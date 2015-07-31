d3265fd98cc301f2b7df31281aa0b57f79e3a583
Baldwin <xuebingbing100@163.com>
2015-07-31 15:42:48 +0800

    添加工厂类

---
+++ WMTSFactory.cpp
@@ -0,0 +1,153 @@
+#include "WMTSFactory.h"
+#include "./win/WinWMTSLayer.h"
+#include "./win/WinWMTSRepository.h"
+#include "./win/WinWMTSLevel.h"
+#include "./win/WinBundle.h"
+#include "./win/WinBundleReader.h"
+
+#include "./hdfs/HdfsWMTSRepository.h"
+#include "./hdfs/HdfsWMTSLayer.h"
+#include "./hdfs/HdfsWMTSLevel.h"
+#include "./hdfs/HdfsBundle.h"
+
+#ifndef NULL
+#define NULL 0
+#endif// #ifndef NULL
+
+using namespace thp;
+
+WMTSFactory* WMTSFactory::_ins = NULL;
+
+WMTSFactory::WMTSFactory()
+{
+	m_eType = WIN32_FILE_SYS;
+}
+
+WMTSFactory::~WMTSFactory()
+{
+	
+}
+
+WMTSFactory* thp::WMTSFactory::Instance()
+{
+	if(NULL == _ins)
+		_ins = new WMTSFactory();
+
+	return _ins;
+}
+
+void thp::WMTSFactory::DeInstance()
+{
+	delete _ins;
+	_ins = NULL;
+}
+
+WMTSRepository* thp::WMTSFactory::createRepository()
+{
+	WMTSRepository* pResult = NULL;
+	switch(m_eType)
+	{
+	case WIN32_FILE_SYS:
+		pResult = new WinWMTSRepository;
+		break;
+
+	case HDFS_SYS:
+		pResult = new HdfsWMTSRepository;
+		break;
+
+	default:
+		break;
+	}
+
+	return pResult;
+}
+
+WMTSLayer* thp::WMTSFactory::createLayer()
+{
+	WMTSLayer* pResult = NULL;
+	switch(m_eType)
+	{
+	case WIN32_FILE_SYS:
+		pResult = new WinWMTSLayer;
+		break;
+
+	case HDFS_SYS:
+		pResult = new HdfsWMTSLayer;
+		break;
+
+	default:
+		break;
+	}
+
+	return pResult;
+}
+
+WMTSLevel* thp::WMTSFactory::createLevel(int nLv)
+{
+	WMTSLevel* pResult = NULL;
+	switch(m_eType)
+	{
+	case WIN32_FILE_SYS:
+		pResult = new WinWMTSLevel(nLv);
+		break;
+
+	case HDFS_SYS:
+		pResult = new HdfsWMTSLevel(nLv);
+		break;
+
+	default:
+		break;
+	}
+
+	return pResult;
+}
+
+Bundle* thp::WMTSFactory::createBundle(const TBundleIDex& tNoEx)
+{
+	Bundle* pResult = NULL;
+	switch(m_eType)
+	{
+	case WIN32_FILE_SYS:
+		pResult = new WinBundle(tNoEx);
+		break;
+
+	case HDFS_SYS:
+		pResult = new HdfsBundle(tNoEx);
+		break;
+
+	default:
+		break;
+	}
+
+	return pResult;
+}
+
+BundleReader* thp::WMTSFactory::createBundleReader()
+{
+	BundleReader* pResult = NULL;
+	switch(m_eType)
+	{
+	case WIN32_FILE_SYS:
+		pResult = new WinBundleReader();
+		break;
+
+	default:
+		break;
+	}
+
+	return pResult;
+}
+
+void thp::WMTSFactory::setFileSys(FST efs)
+{
+	m_eType = efs;
+}
+
+thp::WMTSFactory::FST thp::WMTSFactory::getFileSys()
+{
+	return m_eType;
+}
+
+
+
+
---
+++ WMTSFactory.h
@@ -0,0 +1,55 @@
+/*!
+ * \file WMTSFactory.h
+ * \author Xuebingbing
+ * \brief 工厂类
+ *
+ * TODO: long description
+ * \date 七月 2015
+ * \copyright	Copyright (c) 2015, 北京国科恒通电气自动化科技有限公司\n
+	All rights reserved.
+ */
+#ifndef THP_WMTSFACTORY_H_
+#define THP_WMTSFACTORY_H_
+#include "ParamDef.h"
+
+namespace thp
+{
+	class WMTSRepository;
+	class WMTSLayer;
+	class WMTSLevel;
+	class Bundle;
+	class BundleReader;
+
+	class WMTSFactory
+	{
+	public:
+		/**< 文件系统类型定义 */
+		enum FST
+		{
+			WIN32_FILE_SYS = 0,
+			UNIX_FILE_SYS,
+			HDFS_SYS
+		};
+
+		static WMTSFactory* Instance();
+		static void DeInstance();
+
+		void setFileSys(FST efs);
+		FST getFileSys();
+
+		WMTSRepository* createRepository();
+		WMTSLayer*		createLayer();
+		WMTSLevel*		createLevel(int nLv);
+		Bundle*			createBundle(const thp::TBundleIDex& tNoEx);
+		BundleReader*	createBundleReader();
+
+	private:
+		WMTSFactory();
+		~WMTSFactory();
+
+		static WMTSFactory* _ins;
+		FST m_eType;
+	};
+}
+
+#endif//THP_WMTSFACTORY_H_
